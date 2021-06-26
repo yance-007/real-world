34825915d7edf4891e4f1fd15236bf9455ffe90f
zhangyx20 <477682955@qq.com>
2021-06-26 15:08:45 +0800

    remove 2 files added just now

--- main.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: your name
- * @Date: 2021-06-26 01:48:32
- * @LastEditTime: 2021-06-26 01:48:34
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /real-world/main.js
- */
